49aa913d02f68b1926da48563c704484d85e3716
Philia's Snake <guillaume.maton@brassart.fr>
2024-12-03 09:08:37 +0100

    Initial commit

---
+++ Projet(s)_fait_pour_Brassart/WorkshopVR_(11_11_2024)/MenuManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI quitText;
+    [SerializeField] private TextMeshProUGUI playText;
+    [SerializeField] private GameObject menuCanvas;
+    [SerializeField] private GameObject playCanvas;
+    [SerializeField] private GameObject quitCanvas;
+    [SerializeField] private string sceneName;
+
+    public void Start()
+    {
+        menuCanvas.SetActive(true);
+        playCanvas.SetActive(false);
+        quitCanvas.SetActive(false);
+    }
+
+    public void MenuPlay()
+    {
+        StartCoroutine(ButtonPlay());
+    }
+
+    public void MenuQuit()
+    {
+        StartCoroutine(ButtonQuit());
+    }
+
+    public IEnumerator ButtonPlay()
+    {
+        yield return new WaitForSeconds(0);
+        menuCanvas.SetActive(false);
+        playCanvas.SetActive(true);
+        quitCanvas.SetActive(false);
+        playText.color = Color.blue;
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public IEnumerator ButtonQuit()
+    {
+        yield return new WaitForSeconds(0);
+        menuCanvas.SetActive(false);
+        playCanvas.SetActive(false);
+        quitCanvas.SetActive(true);
+        quitText.color = Color.red;
+        yield return new WaitForSeconds(2);
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
